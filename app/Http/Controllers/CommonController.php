d2a9d168f4307b6b94414c5a7b22a683150c8b07
whdgns0407@gmail.com <whdgns0407@gmail.com>
2023-09-11 11:50:15 +0900

    공동으로 쓸 컨트롤러 정의

---
+++ app/Http/Controllers/CommonController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class CommonController extends Controller
+{
+    //
+}
