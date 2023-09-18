0f0e636734c422558b7c06b8aa4310ee6fc41725
whdgns0407@gmail.com <whdgns0407@gmail.com>
2023-09-18 16:28:11 +0900

    공동 컨트롤러 작성

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -7,8 +7,24 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use Carbon\Carbon;
+
+
+
+
+
+use App\Models\Api_exchange;
+
 
 class CommonController extends Controller
 {
     //
+
+
+    public function price_desc()
+    {
+        $api_exchanges = Api_exchange::select('type', 'korean_name')->orderby('sell1_price', 'desc')->get();
+
+        return $api_exchanges;
+    }
 }
